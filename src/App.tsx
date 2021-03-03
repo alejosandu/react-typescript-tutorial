1f3ac99e8f1ad80d947ce13defe05e96f4c87548
Alejandro Sánchez <alejosandu@gmail.com>
2021-03-02 22:53:47 -0500

    Class 180

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import TodoList from './TodoList'
+import NewTodo from './NewTodo'
 
 const App: React.FC = () => {
 
   const todos = [{id: "1", text: "finish something"}]
 
+  const todosAddHandler = (text: string) => {
+    
+  }
+  
   return (
     <div className="App">
+      <NewTodo onAddTodo={todosAddHandler} />
       <TodoList items={todos} />
     </div>
   );
---
+++ src/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { SyntheticEvent , useRef } from 'react'
+
+interface NewTodoProps {
+    onAddTodo: (text: string) => void
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+
+    const ref = useRef<HTMLInputElement>(null)
+
+    const handleSubmit = (evt: SyntheticEvent) => {
+        evt.preventDefault()
+
+        const { value } = ref.current!
+        props.onAddTodo(value)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="todo-text" >Todo text</label>
+                <input type="text" id="todo-text" ref={ref} />
+            </div>
+            <button type="button" >Add todo</button>
+        </form>
+    )
+}
+
+export default NewTodo
